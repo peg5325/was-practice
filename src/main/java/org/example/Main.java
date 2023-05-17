bea19002e03c1e883380f04d671260368ff20202
Eongyu Park <peg5325@gmail.com>
2023-05-17 16:25:25 +0900

    기본 WebApplicationServer 생성 및 테스트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,10 @@
 package org.example;
 
+import java.io.IOException;
+
+// GET /calculate?operand1=11&operator=*&operand2=55
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
     }
 }
-
-/**
- * echo "# was-practice" >> README.md
- * git init
- * git add README.md
- * git commit -m "first commit"
- * git branch -M main
- * git remote add origin https://github.com/peg5325/was-practice.git
- * git push -u origin main
- */
