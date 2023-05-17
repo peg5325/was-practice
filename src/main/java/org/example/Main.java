166544058c894d5a6b4776354e0d4a744958e400
Eongyu Park <peg5325@gmail.com>
2023-05-17 15:36:30 +0900

    Create WAS-Practice

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/**
+ * echo "# was-practice" >> README.md
+ * git init
+ * git add README.md
+ * git commit -m "first commit"
+ * git branch -M main
+ * git remote add origin https://github.com/peg5325/was-practice.git
+ * git push -u origin main
+ */
